0ca4cab9572278fed43997a457f91e627011f6a9
LazizbekQuvondiqov <165598035+LazizbekQuvondiqov@users.noreply.github.com>
2025-09-15 11:56:59 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,20 @@
+// frontend/src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css'; // Stilni bu yerga ko'chiramiz
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4'; // <-- 1. BU QATORNI IMPORT QILING
+
+// 2. GOOGLE ANALYTICS'NI INITSALIZATSIYA QILING
+// O'zingizning Measurement ID'ngizni shu yerga qo'ying
+const MEASUREMENT_ID = "G-1VRMRJ1HFF"; // <-- BU YERGA O'ZINGIZNING ID'NGIZNI QO'YING
+
+// Faqat production rejimida ishga tushiramiz
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize(MEASUREMENT_ID);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +22,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
