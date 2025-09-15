77c1ad225b61ccf2a2681ede60b9b484847914b9
LazizbekQuvondiqov <165598035+LazizbekQuvondiqov@users.noreply.github.com>
2025-09-15 13:49:54 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,7 @@
-// frontend/src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.css'; // Stilni bu yerga ko'chiramiz
 import App from './App';
-import ReactGA from 'react-ga4'; // <-- 1. BU QATORNI IMPORT QILING
-
-// 2. GOOGLE ANALYTICS'NI INITSALIZATSIYA QILING
-// O'zingizning Measurement ID'ngizni shu yerga qo'ying
-const MEASUREMENT_ID = "G-1VRMRJ1HFF"; // <-- BU YERGA O'ZINGIZNING ID'NGIZNI QO'YING
-
-// Faqat production rejimida ishga tushiramiz
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize(MEASUREMENT_ID);
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
